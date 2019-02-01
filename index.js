810b0323eb72747504954da76c07f0425f3ae389
Nadia27 <jones.nadia.l@gmail.com>
2019-02-01 14:21:30 -0500

    Created separate function for playing sound and added comments

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+// Simon button colors
 var buttonColors = ["red", "blue", "green", "yellow"];
+
+// Randomly selected colors
 var gamePattern = [];
 
+// User selected color pattern
+var userClickPattern = [];
+
+// Randomly select color
+// Push color to gamePattern array
+// FadeOut/FadeIn randomly selected color
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomColorChooser = buttonColors[randomNumber];
   gamePattern.push(randomColorChooser);
   $("#" + randomColorChooser).fadeOut("fast").fadeIn("fast");
-  var colorSound = new Audio("sounds/" + randomColorChooser + ".mp3");
-  console.log(colorSound);
-  colorSound.play();
 }
 
 nextSequence();
 
-console.log(gamePattern);
 
-// Use jQuery to select the button with the same id as the randomChosenColour
+// Detect when any of the buttons are clicked
+$("div.btn").on("click", function() {
+  var userChosenColor = this.id;
+  console.log(userChosenColor);
+  userClickPattern.push(userChosenColor);
+  console.log(userClickPattern);
+});
+
+// Play unique button sounds
+function playSound (name) {
+  var colorSound = new Audio("sounds/" + randomColorChooser + ".mp3");
+  console.log(colorSound);
+  colorSound.play();
+
+}
