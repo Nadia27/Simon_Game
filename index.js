bd8115071cebc41f13776ab4ce8a1ff90b066b4f
Nadia27 <jones.nadia.l@gmail.com>
2019-02-02 14:07:41 -0500

    added keypress event to start game, bug: everytime button pressed is detected game starts even if game has already started

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ var gamePattern = [];
 // User selected color pattern
 var userClickPattern = [];
 
+var level = 0;
+
+// Start Game with any keypress
+$(document).on("keypress",nextSequence);
+
 // Randomly select color
 // Push color to gamePattern array
 // FadeOut/FadeIn randomly selected color
 function nextSequence () {
+  level++;
+  $(".title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4); // random number 0-3
   var randomColorChooser = buttonColors[randomNumber]; // random color from buttonColors array
   gamePattern.push(randomColorChooser); // push randomColor to gamePattern array
@@ -18,9 +25,6 @@ function nextSequence () {
   var colorSound = new Audio("sounds/" + randomColorChooser + ".mp3");
   playSound(colorSound);
 }
-
-nextSequence();
-
 
 // Detect when any of the buttons are clicked
 $("div.btn").on("click", function () {
